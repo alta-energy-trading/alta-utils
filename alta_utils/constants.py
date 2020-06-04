43cba3f72fef76b0a99c8bdb1b8d13b13745227d
Henry Tindal <henryt@arcpet.co.uk>
2020-06-04 14:32:49 +0100

    first commit

---
+++ alta_utils/constants.py
@@ -0,0 +1,9 @@
+EXCHANGE_SERVER = 'https://outlook.office365.com/EWS/Exchange.asmx'
+INFO_EMAIL_ADDRESS = 'henryt@arcpet.co.uk'
+ERROR_EMAIL_ADDRESS = 'henryt@arcpet.co.uk'
+SENDER_EMAIL_ADDRESS = 'targo.support@arcpet.co.uk'
+INTERNAL_DOMAIN = 'Arcpet'
+TARGO_DB_NAME = 'STG_Targo'
+PRICE_DB_NAME = 'Price'
+PRODUCTION_DB_SERVER = 'ArcSql'
+
